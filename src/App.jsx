2b95556c08edf0862d110fd0270678ea0f05b980
suthee <lovalek@gmail.com>
2023-02-22 13:33:05 +0700

    feat: add new ex1 react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+	      <h1>My First React App on Vite</h1>
+      </section>
+    </div>
+  )
+}
+
+export default App
